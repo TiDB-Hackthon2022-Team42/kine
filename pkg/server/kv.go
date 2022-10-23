deb8ace164714b88651b3f8dabc3d6c32e0c3d3a
Rudbeckia.hirta.L <83773602@qq.com>
2022-10-23 17:06:20 +0800

    1

--- pkg/server/kv.go
+++ pkg/server/kv.go
@@ -97,9 +97,9 @@ func (k *KVServerBridge) Put(ctx context.Context, r *etcdserverpb.PutRequest) (*
 	if len(r.Key) == 0 {
 		return nil, errors.New("key is blank")
 	}
-	if r.Key[0] != '/' {
-		return nil, errors.New("key is not a path")
-	}
+	//if r.Key[0] != '/' {
+	//	return nil, errors.New("key is not a path")
+	//}
 	revId, preKv, err := k.limited.backend.Set(ctx, string(r.Key), r.Value, r.Lease)
 	if err != nil {
 		return nil, errors.WithStack(err)
